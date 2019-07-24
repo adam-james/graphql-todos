331a8df2a1fbc1a5903267ea3bf745effc5e8a3f
adam-james <adam.j.sulewski@gmail.com>
2019-07-23 20:09:24 -0600

    load items from server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 import "./App.css";
 
+const QUERY = gql`
+  {
+    allTodoItems {
+      id
+      description
+      done
+    }
+  }
+`;
+
 function App() {
-  return <p>hi</p>;
+  return (
+    <div class="App">
+      <Query query={QUERY}>
+        {({ error, loading, data }) => {
+          if (error) return <p>An error occurred.</p>;
+          if (loading) return <p>Loading...</p>;
+
+          const { allTodoItems } = data;
+
+          return (
+            <ul>
+              {allTodoItems.map(item => (
+                <li key={item.id}>{item.description}</li>
+              ))}
+            </ul>
+          );
+        }}
+      </Query>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://api.graph.cool/simple/v1/cjygl22fm1po10109op2ymvax"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
