8bf041034702fd631eca12a47131f43b8e278241
adam-james <adam.j.sulewski@gmail.com>
2019-07-24 07:40:39 -0600

    add subscription

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import "./App.css";
 
+const SUBSCRIPTION = gql`
+  subscription {
+    TodoItem(filter: { mutation_in: [CREATED] }) {
+      mutation
+      node {
+        id
+        description
+        done
+      }
+    }
+  }
+`;
+
 const QUERY = gql`
   {
     allTodoItems {
@@ -13,26 +26,54 @@ const QUERY = gql`
   }
 `;
 
+class TodoItems extends React.Component {
+  componentDidMount() {
+    this.props.subscribeToNewItems();
+  }
+
+  render() {
+    if (this.props.error) return <p>An error occurred.</p>;
+    if (this.props.loading) return <p>Loading...</p>;
+
+    const { allTodoItems } = this.props.data;
+
+    if (allTodoItems.length < 1) return <p>No items.</p>;
+
+    return (
+      <ol>
+        {allTodoItems.map(item => (
+          <li key={item.id}>{item.description}</li>
+        ))}
+      </ol>
+    );
+  }
+}
+
 function App() {
   return (
-    <div class="App">
+    <div className="App">
       <Query query={QUERY}>
-        {({ error, loading, data }) => {
-          if (error) return <p>An error occurred.</p>;
-          if (loading) return <p>Loading...</p>;
+        {({ subscribeToMore, ...result }) => (
+          <TodoItems
+            {...result}
+            subscribeToNewItems={() => {
+              subscribeToMore({
+                document: SUBSCRIPTION,
+                updateQuery: (prev, { subscriptionData }) => {
+                  if (!subscriptionData.data) return prev;
+                  console.log(subscriptionData);
 
-          const { allTodoItems } = data;
-
-          if (allTodoItems.length < 1) return <p>No items.</p>;
-
-          return (
-            <ol>
-              {allTodoItems.map(item => (
-                <li key={item.id}>{item.description}</li>
-              ))}
-            </ol>
-          );
-        }}
+                  return {
+                    allTodoItems: [
+                      ...prev.allTodoItems,
+                      subscriptionData.data.TodoItem.node
+                    ]
+                  };
+                }
+              });
+            }}
+          />
+        )}
       </Query>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { getMainDefinition } from "apollo-utilities";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { WebSocketLink } from "apollo-link-ws";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: "https://api.graph.cool/simple/v1/cjyh8isgl3dgc0179be7hnhx6"
+});
+
+const wsLink = new WebSocketLink({
+  uri: "wss://subscriptions.graph.cool/v1/cjyh8isgl3dgc0179be7hnhx6",
+  options: {
+    reconnect: true
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
